95c475b4814109a0f003274336b126d7ac04660b
C9b3rD3vi1 <nicksonwe@gmail.com>
2025-04-19 20:00:11 +0300

    Real estate hub url configurationa

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    # This view renders the home page of the listings app
+    return render(request, 'home.html')
+
+def listing(request, listing_id):
+    # This view renders the detail page for a specific listing
+    # The listing_id parameter is used to retrieve the specific listing from the database
+    return render(request, 'listing.html', {'listing_id': listing_id})
